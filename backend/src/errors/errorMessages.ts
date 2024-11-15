fdba3adefd4ef13ee1bcb6fd935284795c0e02a6
ofekbelo <ofek.b123@gmail.com>
2024-11-15 18:46:16 +0200

    Minor fixing in errorMessages enum.

--- backend/src/errors/errorMessages.ts
+++ backend/src/errors/errorMessages.ts
@@ -1,10 +1,13 @@
 enum errorMessages {
-    ERR_REG_001 = 'Server error. Please try again later.',
-    ERR_REG_002 = 'This email is already exist. Plase make the registration with other email.',
-    ERR_REG_003 = 'Your email is not valid.',
-    ERR_REG_004 = 'Your password is not valid. Please type valid password.',
-    ERR_REG_005 = 'Your firstname or lastname is not valid.',
-    ERR_REG_006 = 'One or more of the fields are empty.'
+    SERVER_ERROR = 'Server error. Please try again later.',
+
+    ERR_REG_001 = 'This email is already exist. Plase make the registration with other email.',
+    ERR_REG_002 = 'Your email is not valid.',
+    ERR_REG_003 = 'Your password is not valid. Please type valid password.',
+    ERR_REG_004 = 'Your firstname or lastname is not valid.',
+    ERR_REG_005 = 'One or more of the fields are empty.',
+
+    ERR_LOGIN_001 = 'Email or password incorrect. Please try again',
 }
 
 export default errorMessages;
